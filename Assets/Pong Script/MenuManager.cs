9a6d7801454ca35af78d6e3d91f2eeef6b2f8d42
M.S.R <radityoadit9@gmail.com>
2024-06-25 10:54:31 +0700

    Menu Navigation
    Implementing menu navigation using keyboard.

--- Assets/Pong Script/MenuManager.cs
+++ Assets/Pong Script/MenuManager.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class MenuManager : MonoBehaviour
 {
     [SerializeField]
-    GameObject Menu_Page;
+    GameObject Menu_Page, Play_Page, Credit_Page;
+    [SerializeField]
+    Button PrimaryMenuSelect, PrimaryPlaySelect, PrimaryCreditSelect;
+
+    public void Start()
+    {
+        PrimaryMenuSelect.Select();
+    }
 
     public void OnePlayer_Scene()
     {
@@ -18,8 +27,7 @@ public class MenuManager : MonoBehaviour
         //SceneManager.LoadScene("Pong");
     }
     bool PlayActive;
-    [SerializeField]
-    GameObject Play_Page;
+    
     public void Play()
     {
         PlayActive = !PlayActive;
@@ -28,17 +36,17 @@ public class MenuManager : MonoBehaviour
         {
             Play_Page.SetActive(true);
             Menu_Page.SetActive(false);
+            PrimaryPlaySelect.Select();
         }
         else
         {
             Play_Page.SetActive(false);
             Menu_Page.SetActive(true);
+            PrimaryMenuSelect.Select();
         }
     }
 
     bool CreditActive;
-    [SerializeField]
-    GameObject Credit_Page;
     public void Credit()
     {
         //Debug.Log("Created by M.S.R");
@@ -48,11 +56,13 @@ public class MenuManager : MonoBehaviour
         {
             Credit_Page.SetActive(true);
             Menu_Page.SetActive(false);
+            PrimaryCreditSelect.Select();
         } 
         else
         {
             Credit_Page.SetActive(false);
             Menu_Page.SetActive(true);
+            PrimaryMenuSelect.Select();
         }
     }
     public void OpenUrl()
