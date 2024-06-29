24832bd5d556e8e9ba8887a0869543cb69e60857
M.S.R <radityoadit9@gmail.com>
2024-06-29 15:46:59 +0700

    Floating Text
    Added floating text after hitting power up

--- Assets/Pong Script/PU_PaddleSpeed.cs
+++ Assets/Pong Script/PU_PaddleSpeed.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PU_PaddleSpeed : MonoBehaviour
 {
+    [Header("Item Setting")]
     public PU_Manager PUM;
     public Collider2D ball;
     public GameObject paddle1;
     public GameObject paddle2;
+
+    [Header("Item SFX")]
+    public string PU_Name = "Fast Paddle";
+    public GameObject FloatingTextPrefab;
+    public Collider2D PowerUpCollider;
+    public SpriteRenderer sr;
 
     public void OnTriggerEnter2D(Collider2D other)
     {
@@ -30,7 +38,7 @@ public class PU_PaddleSpeed : MonoBehaviour
                 return;
             }
             //Destroy the object
-            PUM.RemovePowerUp(gameObject);
+            StartCoroutine(BallHit());
         } 
     }
 
@@ -38,9 +46,27 @@ public class PU_PaddleSpeed : MonoBehaviour
     {
         if(gameObject.activeSelf) //Read if game object is active
         {
+            sr = GetComponent<SpriteRenderer>();
+            PowerUpCollider = GetComponent<Collider2D>();
             //Debug.Log(gameObject.name + " is active.");
             StartCoroutine(Timer());
         }
+    }
+
+    void ShowFloatingText()
+    {
+        var go = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+        go.GetComponent<TextMeshPro>().text = PU_Name;
+    }
+
+    public IEnumerator BallHit()
+    {
+        sr.enabled = false;
+        PowerUpCollider.enabled = false;
+        ShowFloatingText();
+        yield return new WaitForSeconds(1.31f);
+        //Destroy the object
+        PUM.RemovePowerUp(gameObject);
     }
 
     private IEnumerator Timer()
--- Assets/Pong Script/PU_SpeedUp.cs
+++ Assets/Pong Script/PU_SpeedUp.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class PU_SpeedUp : MonoBehaviour
 {
+    [Header("Item Setting")]
     public PU_Manager PUM;
     public Collider2D ball;
+
+    [Header("Item SFX")]
+    public string PU_Name = "Fast Ball";
+    public GameObject FloatingTextPrefab;
+    public Collider2D PowerUpCollider;
+    public SpriteRenderer sr;
 
     [SerializeField]
     private float magnitude;
@@ -16,8 +25,7 @@ public class PU_SpeedUp : MonoBehaviour
         if(other == ball)
         {
             ball.GetComponent<Ball>().ActivateSpeedPU(magnitude);
-            //Destroy the object
-            PUM.RemovePowerUp(gameObject);
+            StartCoroutine(BallHit());
         } 
     }
 
@@ -25,9 +33,27 @@ public class PU_SpeedUp : MonoBehaviour
     {
         if(gameObject.activeSelf)
         {
+            sr = GetComponent<SpriteRenderer>();
+            PowerUpCollider = GetComponent<Collider2D>();
             //Debug.Log(gameObject.name + " is active.");
             StartCoroutine(Timer());
         }
+    }
+
+    void ShowFloatingText()
+    {
+        var go = Instantiate(FloatingTextPrefab, transform.position, Quaternion.identity, transform);
+        go.GetComponent<TextMeshPro>().text = PU_Name;
+    }
+
+    public IEnumerator BallHit()
+    {
+        sr.enabled = false;
+        PowerUpCollider.enabled = false;
+        ShowFloatingText();
+        yield return new WaitForSeconds(1.31f);
+        //Destroy the object
+        PUM.RemovePowerUp(gameObject);
     }
 
     private IEnumerator Timer()
